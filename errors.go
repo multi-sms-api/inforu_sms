7b579c066e53875f8aeb77b2174c58666aeef4cc
Ido Kanner <idok@linesip.com>
2019-10-03 14:58:57 +0300

    implementing interface for smshandler

--- errors.go
+++ errors.go
@@ -2,7 +2,6 @@ package inforusms
 
 import (
 	"fmt"
-	"strings"
 )
 
 // SMSError holds information about sending information
@@ -14,23 +13,4 @@ type SMSError struct {
 
 func (e SMSError) Error() string {
 	return fmt.Sprintf("%d: %s", e.Status, e.Description)
-}
-
-// ToError converts XMLResponse to SMSError. If everything is ok, it will return
-// nil
-func ToError(returnStatus XMLResponse) *SMSError {
-	if returnStatus.Status == StatusOK {
-		return nil
-	}
-	result := SMSError{
-		Status:      returnStatus.Status,
-		Description: returnStatus.Description,
-		Effected:    returnStatus.NumberOfRecipients,
-	}
-
-	if strings.HasPrefix(strings.ToLower(result.Description), "error: ") {
-		result.Description = result.Description[7:]
-	}
-
-	return &result
 }
--- response.go
+++ response.go
@@ -1,6 +1,9 @@
 package inforusms
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+	"strings"
+)
 
 // XMLResponse holds the content for the answer arrived back for the
 // request
@@ -24,4 +27,28 @@ func FromXMLResponse(buf []byte) (XMLResponse, error) {
 
 	err := xml.Unmarshal(buf, &response)
 	return response, err
+}
+
+// ToError converts XMLResponse to SMSError. If everything is ok, it will return
+// nil
+func (r XMLResponse) ToError() *SMSError {
+	if r.Status == StatusOK {
+		return nil
+	}
+	result := SMSError{
+		Status:      r.Status,
+		Description: r.Description,
+		Effected:    r.NumberOfRecipients,
+	}
+
+	if strings.HasPrefix(strings.ToLower(result.Description), "error: ") {
+		result.Description = result.Description[7:]
+	}
+
+	return &result
+}
+
+// IsOK Implements the interface to know if response holds success or not
+func (r XMLResponse) IsOK() bool {
+	return r.Status == StatusOK
 }
